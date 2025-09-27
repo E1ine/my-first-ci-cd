12cf8ba4f6f02feac8a808bf497011286547e86f
Eline <danparishev.03@mail.ru>
2025-09-27 21:14:43 +0300

    fix: upadte flask to a secure version

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import pytest
+from app import app
+
+def test_hello_world():
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
+        assert b'Hello' in response.data
