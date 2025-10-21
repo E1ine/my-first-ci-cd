3aeb759082adc7031bc6f02dc85d7664f03292ea
danil <danparishev.03@mail.ru>
2025-10-22 00:04:41 +0300

    chore: add changes fails from flake8

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
@@ -9,6 +11,7 @@ def hello_world():
     except (ValueError, TypeError):
         user_id = 1
     return f'<h1>Hello, user #{user_id}!</h1>'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
