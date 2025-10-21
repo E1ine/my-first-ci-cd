f958f705fd602777e7813dc720827165678d101f
danil <danparishev.03@mail.ru>
2025-10-22 00:58:54 +0300

    demo: introduce nosec comm

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,5 +4,5 @@ from app import app
 def test_hello_world():
     with app.test_client() as client:
         response = client.get('/')
-        assert response.status_code == 200 # nosec
+        assert response.status_code == 200 #  nosec
         assert b'Hello' in response.data
